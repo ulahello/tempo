a3853641d660af78b6f5981d8b9594f5eb2121b5
Ula Shipman <ula.hello@mailbox.org>
2025-04-04 14:33:04 -0700

    feat(buffer): [WIP] add some length-based pre/postconditions

--- src/ring_buffer_core.ads
+++ src/ring_buffer_core.ads
@@ -20,33 +20,56 @@ is
 
    type Buffer (Max_Capacity : Capacity_Type) is private;
 
-   function Buffer_Init (Max_Capacity : Capacity_Type) return Buffer;
+   function Buffer_Init (Max_Capacity : Capacity_Type) return Buffer
+   with
+     Post =>
+       Is_Empty (Buffer_Init'Result)
+       and then Buffer_Init'Result.Max_Capacity = Max_Capacity;
 
    --  Buffer operations
 
-   function Length (B : Buffer) return Length_Type;
+   function Length (B : Buffer) return Length_Type
+   with Post => Length'Result in 0 .. B.Max_Capacity;
 
-   function Is_Empty (B : Buffer) return Boolean;
+   function Is_Empty (B : Buffer) return Boolean
+   with Post => Is_Empty'Result = (Length (B) = 0);
 
-   function Is_Full (B : Buffer) return Boolean;
+   function Is_Full (B : Buffer) return Boolean
+   with Post => Is_Full'Result = (Length (B) = B.Max_Capacity);
 
    function Get (B : Buffer; I : Index_Type) return Element
    with Pre => I in 1 .. Length (B);
 
    procedure Push (B : in out Buffer; V : Element)
-   with Pre => not Is_Full (B);
+   with Pre => not Is_Full (B), Post => Length (B) = Length (B'Old) + 1;
 
    --  Like Push, but will Pop to make room for the element.
-   procedure Push_Out (B : in out Buffer; V : Element);
+   procedure Push_Out (B : in out Buffer; V : Element)
+   with
+     Post =>
+       (if Is_Full (B'Old) then Length (B) = Length (B'Old)
+        else Length (B) = Length (B'Old) + 1);
 
    procedure Pop (B : in out Buffer; V : out Element)
-   with Pre => not Is_Empty (B);
+   with Pre => not Is_Empty (B), Post => Length (B) = Length (B'Old) - 1;
 
-   procedure Clear (B : in out Buffer);
+   procedure Clear (B : in out Buffer)
+   with Post => Is_Empty (B);
 
-   procedure Truncate_Back (B : in out Buffer; Max_Length : Length_Type);
+   procedure Truncate_Back (B : in out Buffer; Max_Length : Length_Type)
+   with
+     Post =>
+       (if Length (B'Old) <= Max_Length then Length (B) = Length (B'Old)
+        else Length (B) = Max_Length);
 
 private
+
+   --  TODO(ula): i might need to make these public and define
+   --  preconditions and postconditions based on the read and write
+   --  indices (or at least factor those in). otherwise i see no way
+   --  to prove that Length is correct. the validity of these indices
+   --  both before and after also becomes an important invariant of
+   --  several methods.
 
    type Element_Array is array (Positive range <>) of Element;
 
